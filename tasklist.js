2060c4a2a693041b031f025f028051cd926a9c26
minad <mail@daniel-mendler.de>
2011-04-13 18:38:42 +0200

    use javascript closure

--- tasklist.js
+++ tasklist.js
@@ -1,87 +1,85 @@
-function taskListCollapse(element) {
-    var collapsed = element.collapsed = !element.collapsed;
-    var clazz = element.getAttribute('class');
-    if (collapsed) {
-	clazz = clazz.replace(/\s*tlSelected\s*/, '');
-    } else {
-	clazz += ' tlSelected';
-    }
-    element.setAttribute('class', clazz);
-    element = element.nextSibling;
-    while (element) {
-	if (element.nodeType === 1 && element.nodeName === 'TR') {
-	    if (element.getAttribute('class').indexOf('tlProject') !== -1) {
-		break;
-	    }
-	    element.style.display = collapsed ? 'none' : 'table-row';
+(function() {
+    function collapse(element) {
+	var collapsed = element.collapsed = !element.collapsed;
+	var clazz = element.getAttribute('class');
+	if (collapsed) {
+	    clazz = clazz.replace(/\s*tlSelected\s*/, '');
+	} else {
+	    clazz += ' tlSelected';
 	}
+	element.setAttribute('class', clazz);
 	element = element.nextSibling;
-    }
-}
-
-var taskListLast = null;
-
-function taskListToggle(element) {
-    taskListCollapse(element);
-    if (taskListLast && taskListLast !== element) {
-	taskListCollapse(taskListLast);
-    }
-    taskListLast = element.collapsed ? null : element;
-    location.href = element.collapsed ? '#none' : element.hash;
-}
-
-function taskListFilterSubmit() {
-    this.form.action = location.hash;
-    this.form.submit();
-}
-
-function taskListSelectStart() {
-    return false;
-}
-
-function taskListToggleThis() {
-    taskListToggle(this);
-}
-
-function taskListInit() {
-    var index = 0;
-    var openElement = null;
-    var elements = document.getElementsByClassName('tlProjects');
-    var i, j;
-
-    for (j = 0; j < elements.length; ++j) {
-	var element = elements[j];
-
-	var children = element.getElementsByTagName('tr');
-	for (i = 0; i < children.length; ++i) {
-	    var child = children[i];
-
-	    if (child.getAttribute('class').indexOf('tlProject') !== -1) {
-		child.hash = '#' + index;
-		child.collapsed = true;
-		child.onclick = taskListToggleThis;
-		child.onselectstart = taskListSelectStart;
-		child.unselectable = 'on';
-		child.style.MozUserSelect = 'none';
-
-		if (location.hash === child.hash) {
-		    openElement = child;
+	while (element) {
+	    if (element.nodeType === 1 && element.nodeName === 'TR') {
+		if (element.getAttribute('class').indexOf('tlProject') !== -1) {
+		    break;
 		}
-		++index;
-	    } else {
-		child.style.display = 'none';
+		element.style.display = collapsed ? 'none' : 'table-row';
 	    }
+	    element = element.nextSibling;
 	}
     }
 
-    if (openElement) {
-	taskListToggle(openElement);
+    var currentOpenElement = null;
+
+    function toggle() {
+	collapse(this);
+	if (currentOpenElement && currentOpenElement !== this) {
+	    collapse(currentOpenElement);
+	}
+	currentOpenElement = this.collapsed ? null : this;
+	location.href = this.collapsed ? '#none' : this.hash;
     }
 
-    elements = document.getElementsByClassName('tlFilter');
-    for (i = 0; i < elements.length; ++i) {
-	elements[i].onchange = taskListFilterSubmit;
+    function submitElement() {
+	this.form.action = location.hash;
+	this.form.submit();
     }
-}
 
-addOnloadHook(taskListInit);
+    function noSelect() {
+	return false;
+    }
+
+    function init() {
+	var index = 0;
+	var openElement = null;
+	var elements = document.getElementsByClassName('tlProjects');
+	var i, j;
+
+	for (j = 0; j < elements.length; ++j) {
+	    var element = elements[j];
+
+	    var children = element.getElementsByTagName('tr');
+	    for (i = 0; i < children.length; ++i) {
+		var child = children[i];
+
+		if (child.getAttribute('class').indexOf('tlProject') !== -1) {
+		    child.hash = '#' + index;
+		    child.collapsed = true;
+		    child.onclick = toggle;
+		    child.onselectstart = noSelect;
+		    child.unselectable = 'on';
+		    child.style.MozUserSelect = 'none';
+
+		    if (location.hash === child.hash) {
+			openElement = child;
+		    }
+		    ++index;
+		} else {
+		    child.style.display = 'none';
+		}
+	    }
+	}
+
+	if (openElement) {
+	    toggle.call(openElement);
+	}
+
+	elements = document.getElementsByClassName('tlFilter');
+	for (i = 0; i < elements.length; ++i) {
+	    elements[i].onchange = submitElement;
+	}
+    }
+
+    addOnloadHook(init);
+})();
