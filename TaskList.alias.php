d6e8836221973d455ce4983e9bbba058d14e4cc4
minad <mail@daniel-mendler.de>
2011-04-13 08:37:40 +0200

    remove trailing ws

--- TaskList.alias.php
+++ TaskList.alias.php
@@ -1,11 +1,11 @@
 <?php
 $aliases = array();
- 
+
 $aliases['en'] = array(
     'NewTask' => array('New Task'),
     'NewProject' => array('New Project'),
 );
- 
+
 $aliases['de'] = array(
     'NewTask' => array('Neue Aufgabe', 'NeueAufgabe'),
     'NewProject' => array('Neues Projekt', 'NeuesProjekt'),
